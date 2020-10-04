632e781fae8fb536fc8ad88402d7dd8261c1662e
Darko Todoroski <daredarepp@yahoo.com>
2020-10-04 21:58:33 +0200

    Spin cheese

--- js/app.js
+++ js/app.js
@@ -1,10 +1,22 @@
 window.addEventListener('load', () => {
   const menuBtn = document.querySelector('.js-menu-btn');
   const header = document.querySelector('.js-header');
+  const cheese = document.querySelector('.js-cheese');
 
   menuBtn.addEventListener('click', toggleMenu);
+  cheese.addEventListener('click', spinCheese);
+  cheese.addEventListener('click', spinCheese);
 
   function toggleMenu() {
     header.classList.toggle('active');
+  }
+
+  function spinCheese() {
+    if (cheese.classList.contains('animatable')) {
+      return
+    } else {
+      cheese.classList.add('animatable');
+      cheese.addEventListener('animationend', () => cheese.classList.remove('animatable'));
+    }
   }
 })
